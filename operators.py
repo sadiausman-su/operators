f62053654d7e73197c49507f72fb840742814051
sadiausman-su <usmanbodla047@gmail.com>
2025-03-08 05:46:53 +0000

    Added operators program

---
+++ operators.py
@@ -0,0 +1,15 @@
+#operators
+#operators are special symbols that perform operations on variables and vlaues.they can b categorized into several types.
+
+
+#Arithmetic operators
+x=10
+y=20
+print(x+y)    #for addition
+print(x-y)     #for subtract
+print(x*y)      #for multiply
+print(x/y)      #for division
+print(x%y)      #for remainder
+print(x**y)     #for exponent
+print(x//y)     #for floor division
+
